f387fddefb92a729d3176d2186babc3e9c13e46b
elyessassi <mjoxen@gmail.com>
2026-07-29 09:58:23 +0000

    fixing typo in filter

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois -H $1 | awk '/^(Tech |Registrar |Admin)/ {sub(": ?", ","); sub("Ext,", "Ext:,"); sub("\n", "");  print}' > $1.csv
+whois -H $1 | awk '/^(Tech |Registrant |Admin)/ {sub(": ?", ","); sub("Ext,", "Ext:,"); sub("\n", "");  print}' > $1.csv
