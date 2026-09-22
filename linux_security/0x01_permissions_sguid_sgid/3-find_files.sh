d4db84d96adf2998466261bd113de64d1748e60c
elyessassi <mjoxen@gmail.com>
2026-09-22 15:48:57 -0500

    passing error messages to /dev/null

--- linux_security/0x01_permissions_sguid_sgid/3-find_files.sh
+++ linux_security/0x01_permissions_sguid_sgid/3-find_files.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /dev/null | sudo find $1 \( -perm -4000 -o -perm -2000 \) -exec ls -l {} \; | tee > /dev/null
+cat /dev/null | sudo find $1 \( -perm -4000 -o -perm -2000 \) -exec ls -l {} \; 2> /dev/null
